8004a3b018e7b34ac41d17164cd080bce9f9a396
sayhellotovanessa <vanessa.guedes@britny.se>
2017-01-09 16:36:11 +0100

    exercising escape sequences

---
+++ count5infinite.py
@@ -0,0 +1,3 @@
+while True:
+    for i in ["1","2","3","4","5"]:
+        print "%s\r" % i,
---
+++ months.py
@@ -0,0 +1,3 @@
+months_py = "\vJan\vFeb\vMar\vApr\vMay\vJun\vJul\vAug\vSep\vOct\vNov\vDec"
+
+print months_py
